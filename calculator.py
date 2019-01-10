089c6e5330be595b07ce5f6eda71c22667264d91
Eoin <eoinom@gmail.com>
2019-01-10 18:54:26 +0000

    Add my first calculator with no tests.

---
+++ calculator.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 10 18:37:58 2019
+
+@author: eoino
+"""
+
+def add(first, second):
+    return first + second
+
+
+def subtract(first, second):
+    return first - second
+
+print(add(2, 2))
+
+print(add(2, 3))
+
+print(subtract(2, 2))
