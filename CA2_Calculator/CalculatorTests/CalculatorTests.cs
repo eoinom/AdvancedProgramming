ba14556d7e208a24a57d7e075f95c341f4398af4
Eoin <eoinom@gmail.com>
2019-01-23 23:26:59 +0000

    Tests added for Add, Divide, Multiply and Subtract.

--- CA2_Calculator/CalculatorTests/CalculatorTests.cs
+++ CA2_Calculator/CalculatorTests/CalculatorTests.cs
@@ -7,8 +7,67 @@ namespace CalculatorTests
     public class CalculatorTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Add_2Plus5_Equals7()
         {
+            Calculator calc = new Calculator();
+            var result = calc.Add(2, 5);
+            Assert.AreEqual(7, result);
+        }
+
+        [TestMethod]
+        public void Add_2PlusNegative5_EqualsNegative3()
+        {
+            Calculator calc = new Calculator();
+            var result = calc.Add(2, -5);
+            Assert.AreEqual(-3, result);
+        }
+
+        [TestMethod]
+        public void Divide_25DividedBy2_Equals12Point5()
+        {
+            Calculator calc = new Calculator();
+            var result = calc.Divide(25, 2);
+            Assert.AreEqual(12.5, result);
+        }
+
+        [TestMethod]
+        public void Divide_200DividedByNegative10_EqualsNegative20()
+        {
+            Calculator calc = new Calculator();
+            var result = calc.Divide(200, -10);
+            Assert.AreEqual(-20, result);
+        }
+
+        [TestMethod]
+        public void Multiply_Negative5Point2MultiplyBy2_EqualsNegative10Point4()
+        {
+            Calculator calc = new Calculator();
+            var result = calc.Multiply(-5.2, 2);
+            Assert.AreEqual(-10.4, result);
+        }
+
+        [TestMethod]
+        public void Multiply_1000MultiplyByZero_EqualsZero()
+        {
+            Calculator calc = new Calculator();
+            var result = calc.Multiply(1000, 0);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Subtract_10Minus4_Equals6()
+        {
+            Calculator calc = new Calculator();
+            var result = calc.Subtract(10, 4);
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void Subtract_10MinusNegative10_Equals20()
+        {
+            Calculator calc = new Calculator();
+            var result = calc.Subtract(10, -10);
+            Assert.AreEqual(20, result);
         }
     }
 }
