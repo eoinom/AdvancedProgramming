577daf5a96434ced92af18c1e5dcb973460b6243
Eoin <eoinom@gmail.com>
2019-02-08 22:46:15 +0000

    Added HorseBet Class

---
+++ CA3_HorseBetTracking/HorseBet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA3_HorseBetTracking
+{
+    class HorseBet
+    {
+        public string RaceCourse { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Price { get; set; }
+        public bool BetWon { get; set; }
+
+        public HorseBet() { }
+
+        public HorseBet(string raceCourse, DateTime date, decimal price, bool betWon)
+        {
+            RaceCourse = raceCourse;
+            Date = date;
+            Price = price;
+            BetWon = betWon;
+        }
+
+        public override string ToString()
+        {
+            string betSuccess = BetWon ? "Won" : "Lost";
+            return RaceCourse + ", " + Date.ToShortDateString() + ", " + Price.ToString() +
+                ", " + betSuccess;
+        }
+    }
+}
